b06ebc8c8d2dcdb9e118c8b062ec1684b143b3b1
Aiir5 <123135375+Aiir5@users.noreply.github.com>
2026-07-26 18:35:31 +0800

    Bonus files

---
+++ Bonus Template/Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.set_page_config(
+    page_title="Home"
+)
+
+
+st.header("Welcome to . . .")
+
+st.write("Home page")
---
+++ Bonus Template/pages/Sample page 1.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Example page 1"
+)
+
+st.header("Example page 1")
